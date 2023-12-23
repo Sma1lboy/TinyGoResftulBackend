a5fdcc45dbe03b7f04a0856cc6446bf63c9539d3
Sma1lboy <541898146chen@gmail.com>
2023-12-23 03:23:30 -0600

    using gin html render tech

--- Main.go
+++ Main.go
@@ -6,23 +6,27 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
 
 func indexHandler(c *gin.Context) {
-	file, err := os.Open("./index.html")
-	fmt.Println("opened file")
-	if err != nil {
-		panic(err.Error())
-	}
-	defer file.Close()
-	bytes, err := io.ReadAll(file)
-	if err != nil {
-		panic(err.Error())
+	//old school way to loading html page
+	// file, err := os.Open("./index.html")
+	// fmt.Println("opened file")
+	// if err != nil {
+	// 	panic(err.Error())
+	// }
+	// defer file.Close()
+	// bytes, err := io.ReadAll(file)
+	// if err != nil {
+	// 	panic(err.Error())
 
-	}
-	fmt.Fprintln(c.Writer, string(bytes))
+	// }
+	// fmt.Fprintln(c.Writer, string(bytes))
+
+	c.HTML(http.StatusOK, "index.html", gin.H{"users": getAllUsers()})
 }
 func cssHandler(c *gin.Context) {
 	file, err := os.Open("./style.css")
@@ -48,9 +52,13 @@ func addUserHandler(c *gin.Context) {
 	addUser(user)
 }
 
-// func userRemove(res http.ResponseWriter, req *http.Request) {
+func removeUserHandler(c *gin.Context) {
+	idStr := c.Query("id")
+	id, _ := strconv.Atoi(idStr)
+	removeUser(id)
+	c.JSON(http.StatusOK, gin.H{})
+}
 
-// }
 // func userUpdate(res http.ResponseWriter, req *http.Request) {
 
 // }
@@ -59,12 +67,13 @@ func getAllHandler(c *gin.Context) {
 }
 func main() {
 	r := gin.Default()
+	r.Static("/static", "./static/")
+	r.LoadHTMLGlob("./templates/*")
 	r.GET("/", indexHandler)
-	r.GET("/style.css", cssHandler)
-
 	//getall
 	r.GET("/user/all", getAllHandler)
 	r.POST("/user/add", addUserHandler)
+	r.GET("/user/remove/:id", removeUserHandler)
 
 	if err := r.Run(":8000"); err != nil {
 		panic(err)
